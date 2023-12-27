234a11f898a735e4534b9542d32d58baa4e62702
goatvette <149712504+goatvette@users.noreply.github.com>
2023-12-27 20:10:45 +0400

    Update ProductEditScreen.jsx

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -271,14 +271,9 @@ const ProductEditScreen = () => {
               ></Form.Control>
             </Form.Group>
 
-            <Form.Group controlId='description'>
-              <Form.Label className='text-black'>Description</Form.Label>
-              <Form.Control
-                type='text'
-                placeholder='Enter description'
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-              ></Form.Control>
+           <Form.Group controlId='description'>
+              <Form.Label className='text-black'>Description (Type // to create a new para)</Form.Label>
+              <Form.Control as='textarea' rows={10} placeholder='Enter description' value={description} onChange={e => setDescription(e.target.value)}></Form.Control>
             </Form.Group>
 
             <div className='d-grid'>
